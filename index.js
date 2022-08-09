a3c3fc69065c06271636b617d7d22ef872191950
Stanisław Jakubowski <sjakubowski3554@gmail.com>
2022-08-09 22:33:37 +0200

    add header, city and price scrapping

--- index.js
+++ index.js
@@ -1,1 +1,36 @@
-console.log('first commit')
+import axios from 'axios'
+import * as cheerio from 'cheerio'
+import fs from 'fs'
+
+const stream = fs.createWriteStream('append.txt', { flags: 'a' })
+
+const items = []
+
+const fetch = async () => {
+	try {
+		const { data } = await axios.get('https://www.olx.pl/d/oferty/q-yeezy/?search%5Border%5D=created_at:desc')
+		const $ = cheerio.load(data)
+
+		// remove span with text and style tags
+		$('.css-e2218f').remove()
+		$('style').remove()
+
+		// iterate through listings
+		$('.css-u2ayx9').each((i, el) => {
+			const heading = $(el).find('h6').text()
+			const city = $(el).next().text().split('-')[0]
+			const price = $(el).text().slice(heading.length)
+
+			// check if listing is already in the array
+			if (!items.includes(heading)) {
+				console.log(`Added listing: ${heading} ${price}`)
+				items.push(heading)
+				stream.write('Title: ' + heading + '\n' + 'Price: ' + price + '\n' + 'City: ' + city + '\n' + '\n' + '\n')
+			}
+		})
+	} catch (err) {
+		console.log(err)
+	}
+}
+
+setInterval(() => fetch(), 5000)
